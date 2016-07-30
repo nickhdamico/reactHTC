536903c8eac006d032a90ca6b09ebefc89b76ecb
gerutz <germanutz@gmail.com>
2016-07-30 03:42:14 -0300

    commit para compartir con mac

--- base/app/components/Main/VideoBG.jsx
+++ base/app/components/Main/VideoBG.jsx
@@ -6,8 +6,8 @@ class VideoBG extends Component {
   render(){
     return(
       <div style={styles.base}>
-        <video autoPlay loop >
-            <source src={require("../Assets/coder.mp4")}/>
+        <video style={styles.video} >
+
         </video>
       </div>
     );
@@ -20,6 +20,18 @@ const styles = {
     backgroundColor : "green",
     width : '100%',
     minHeight : '400px'
+  },
+  video : {
+    position: 'absolute',
+     zIndex: 0,
+     background: 'url(../Assets/coder.mp4) no-repeat',
+     backgroundSize: '100% 100%',
+     top: '0px',
+     left: '0px', /* fixed to left. Replace it by right if you want.*/
+     minWidth: '100%',
+     minHeight: '100%',
+     width: 'auto',
+     height: 'auto'
   }
 }
 
